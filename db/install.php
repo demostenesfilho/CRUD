c1c97f80b86817312a0257e51c2db36e1c4ab8ff
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2020-10-23 13:33:56 -0300

    Add files via upload

---
+++ db/install.php
@@ -0,0 +1,81 @@
+<?php
+    require('db.php');
+
+    function criarbanco($nomedb) {
+        $CONN = conexao();
+        $sql = "CREATE OR REPLACE DATABASE " . $nomedb;
+
+        $resultado = mysqli_query($CONN, $sql);
+
+        if (!$resultado)
+            die ("Erro: Criação Database..." . mysqli_error($CONN). "<br />");
+        
+        echo "DB: Database criado... <br />";
+        mysqli_close($CONN);
+    }
+
+    function criartabelausuario($nomedb) {
+        echo "Criando tabela... <br />";
+        $CONN = conexao();
+
+        $sql = "USE " . $nomedb . ";";
+        $resultado = mysqli_query($CONN, $sql);
+
+        if (!$resultado)
+            die ("Erro: Seleção database..." . mysqli_error($CONN). "<br />");
+        
+        echo "DB: Database selecionada... <br />";
+
+        $sql = "CREATE TABLE usuario (";
+        $sql .= "codigo INT NOT NULL AUTO_INCREMENT,";
+        $sql .= "nome VARCHAR(250),";
+        $sql .= "senha VARCHAR(100),";
+        $sql .= "PRIMARY KEY (codigo)";
+        $sql .= ");";
+
+        $resultado = mysqli_query($CONN, $sql);
+
+        if (!$resultado)
+            die ("Erro: Criação Tabela..." . mysqli_error($CONN). "<br />");
+        
+        echo "DB: Tabela criada... <br />";
+        mysqli_close($CONN);
+    }
+
+    function popularusuario($nomedb) {
+        echo "Populando tabela... <br />";
+        $CONN = conexao();
+
+        $sql = "USE " . $nomedb . ";";
+        $resultado = mysqli_query($CONN, $sql);
+
+        if (!$resultado)
+            die ("Erro: Seleção database..." . mysqli_error($CONN). "<br />");
+
+            echo "DB: Database selecionada... <br />";
+
+            $sql = "INSERT INTO usuario (nome, senha)";
+            $sql .= "VALUES ('Fulano de Tal', '12345');";
+            $sql .= "INSERT INTO usuario (nome, senha)";
+            $sql .= "VALUES ('Beltrano de Tal', '54321');";
+            $sql .= "INSERT INTO usuario (nome, senha)";
+            $sql .= "VALUES ('Cicrano de Tal', '67890');";
+
+            $resultado = mysqli_multi_query($CONN, $sql);
+
+            if (!$resultado)
+            die ("Erro: Polulação da tabela usuario... " . mysqli_error($CONN). "<br />");
+
+            echo "DB: Populado tabela usuario... <br />";
+            mysqli_close($CONN);
+    }
+
+    criarbanco("agenda");
+    criartabelausuario("agenda");
+    popularusuario("agenda");
+
+
+
+
+
+?>
