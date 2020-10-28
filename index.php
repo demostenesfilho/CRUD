84913024b32abdc507e5503b1c7c8be86d274f7b
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2020-10-28 17:06:17 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-    <a href="dashboard.php">Dashboard</a>
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <a href="dashboard.php">Dashboard</a>
+</body>
+</html>
